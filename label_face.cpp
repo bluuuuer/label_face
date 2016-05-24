8be1f08675e78240a1317c2d02d0c484a1ba527d
nr <shibuyanorailgun@foxmail.com>
2016-05-24 17:19:27 +0800

    align outputs

--- label_face.cpp
+++ label_face.cpp
@@ -85,7 +85,7 @@ redraw:
     int kb = waitKey(0);
     switch (kb) {
     case 1048678: // 'f'
-      printf("%-8d %s", off, filepaths[i].c_str());
+      printf("%-5d %-20s", i, filepaths[i].c_str());
       for (int i = 0; i < N_LANDMARK; ++i) {
         printf("%5d %5d", g_xs[i], g_ys[i]);
       }
